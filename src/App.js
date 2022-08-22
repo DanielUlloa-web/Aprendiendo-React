628d06dd0093fdd8d1e089ce6650bceebc66392e
Daniel Ulloa <daniel.ulloahoyos@gmail.com>
2022-08-22 08:29:52 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} alt='logo' className='logo'></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on Github</li>
+          <li>is maintained by Facebook</li>
+          <li>Power thousands of enterprice apps, including mobile apps</li>
+        </ul>
       </header>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
